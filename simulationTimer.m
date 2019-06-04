f306bcd3850c4f8654a8540a2e34257ca7279e2d
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2019-06-04 18:27:29 -0400

    removed whitespace

--- simulationTimer.m
+++ simulationTimer.m
@@ -1,12 +1,7 @@
 function [totalTime, experimentalPi] = simulationTimer (N)
 
     t = time();
-    % ------------------- CODE TO TIME HERE -----------------------------
-
     experimentalPi = fastSimulation(N);
-
-    % -------------------------------------------------------------------
-
     totalTime = time() - t;
 
 end;
