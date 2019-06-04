205d67d0a402f1db146e0ad6a864a5dcf8380879
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2019-06-04 19:15:20 -0400

    implemented linear regression for simulation time vs iterations

--- convergencePlot.m
+++ convergencePlot.m
@@ -1,12 +1,25 @@
-function [pis, times] = convergencePlot(Ns = 1:100)
-    pis = zeros(1, size(Ns, 2));
-    times = zeros(1, size(Ns, 2));
+function [pis, times, theta] = convergencePlot(Ns = 1:100)
+    pis = zeros(size(Ns, 2), 1);
+    times = zeros(size(Ns, 2), 1);
 
     for i=1:size(Ns, 2)
         [totalTime, experimentalPi] = simulationTimer(Ns(i));
         pis(i) = experimentalPi;
         times(i) = totalTime;
     end;
+
+    iterations = 1000;
+    alpha = 0.000001;
+    [theta, J_history] = linearRegression(Ns', times, alpha, iterations);
+    ys = zeros(1, size(Ns, 2));
+    for i=1:size(Ns, 2)
+        ys(i) = linearFunction(Ns(i), theta);
+    end;
+
+    figure;
+    plot(1:iterations, J_history);
+    xlabel('Number of Iterations of Linear Regression');
+    ylabel('Cost Function');
 
     figure;
     plot(Ns, pis, Ns, ones(1, size(Ns, 2)) * pi, Ns, ones(1, size(Ns, 2)) * mean(pis));
@@ -15,8 +28,8 @@ function [pis, times] = convergencePlot(Ns = 1:100)
     ylabel('Experimental Pi');
 
     figure;
-    plot(Ns, times, Ns, ones(1, size(Ns, 2)) * mean(times));
-    legend('Simulation Time', 'Average Simulation Time');
+    plot(Ns, times, Ns, ones(1, size(Ns, 2)) * mean(times), Ns, ys);
+    legend('Simulation Time', 'Average Simulation Time', 'Best Fit Line');
     xlabel('Number of Iterations');
     ylabel('Simulation Time');
 
--- main.m
+++ main.m
@@ -4,5 +4,6 @@ N = 100;
 printf('\nThe value of PI is %f\n', pi);
 printf('The experimental value of PI is %f with %.0f iterations\n', experimentalPi, N);
 
-[pis, times] = convergencePlot(1:N * 10);
+[pis, times, theta] = convergencePlot(1:N * 10);
 printf('The average experimental value of PI is %f with iterations ranging from 1 to %.0f and it took %fs\n\n', mean(pis), N*10, sum(times));
+printf('The simulation time approximately followed the function y = %.3f + %.3fx\n', theta(1), theta(2));
